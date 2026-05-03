023ac12b6c425fbcf69d9d7d1525c22929d70f54
noxuxgg <fernandohuanca2005@gmail.com>
2026-05-03 18:15:20 -0400

    Creando login

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,14 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+
+export interface Usuario{
+  nombreUsuario: string,
+  contrasenia: string
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  urlBase = "https://laravue2.blumbit.net/back/public/api";
+  urlBase = environment.servidor;
   http = inject(HttpClient);
 
   funListar(){
     return this.http.get(`${this.urlBase}/usuarios`);
+  }
+
+  funGuardar(dato: Usuario){
+    return this.http.post(`${this.urlBase}/usuarios`, dato);
+  }
+
+  funEditar(dato: Usuario, id: string){
+    return this.http.patch(`${this.urlBase}/usuarios/${id}`, dato)
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    servidor: 'http://localhost:3000'
+};
